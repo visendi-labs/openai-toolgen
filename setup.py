8592247c31978f8559f19d6516f5298fd3fffc73
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-11-20 13:52:04 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openai-toolgen",
+    version="0.1.0",
+    author="Rasmus Nordström",
+    author_email="nordstrom.rasmus@gmail.com",
+    description="A library for generating tools for OpenAI projects",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/visendi-labs/openai-toolgen",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

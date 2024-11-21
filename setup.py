954ddf4ed17b4c2c617a89977ca7cd29821c4aef
Rasmus Nordström <rasmus.nordstrom@lotsgroup.com>
2024-11-21 10:59:35 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openai-toolgen",
-    version="0.2.1",
+    version="0.3.0",
     author="Rasmus Nordström",
     author_email="nordstrom.rasmus@gmail.com",
     description="A library for generating tools for OpenAI projects",
